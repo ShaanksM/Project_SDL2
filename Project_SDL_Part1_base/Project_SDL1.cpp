f962a1cf399714f6495fb743fc78e825740ffcf6
ShaanksM <boukritmohamad@gmail.com>
2021-10-21 08:14:38 -0400

    Update : Afficher moutons + loup randomly

---
+++ Project_SDL_Part1_base/Project_SDL1.cpp
@@ -0,0 +1,96 @@
+﻿#include "Project_SDL1.h"
+
+#include <algorithm>
+#include <cassert>
+#include <cstdlib>
+#include <numeric>
+#include <random>
+#include <string>
+
+void init() {
+  // Initialize SDL
+  if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO) < 0)
+    throw std::runtime_error("init():" + std::string(SDL_GetError()));
+
+  // Initialize PNG loading
+  int imgFlags = IMG_INIT_PNG;
+  if (!(IMG_Init(imgFlags) & imgFlags))
+    throw std::runtime_error("init(): SDL_image could not initialize! "
+                             "SDL_image Error: " +
+                             std::string(IMG_GetError()));
+}
+
+namespace {
+// Defining a namespace without a name -> Anonymous workspace
+// Its purpose is to indicate to the compiler that everything
+// inside of it is UNIQUELY used within this source file.
+
+SDL_Texture* load_texture_for(const std::string& path,
+                              SDL_Renderer* renderer) {
+  SDL_Texture* loaded_image = IMG_LoadTexture(renderer, path.c_str());
+
+  if (loaded_image == nullptr) {
+    throw std::runtime_error("Unable to load image");
+  } 
+  return loaded_image;
+}
+
+int get_random_int_in_range(int min, int max) {
+  std::random_device rd;
+  std::mt19937 rng(rd());
+  std::uniform_int_distribution<int> uni(min, max);
+  return uni(rng);
+}
+}
+
+application::application(unsigned n_sheep, unsigned n_wolf) // config de la fenetre
+{
+  SDL_CreateWindowAndRenderer(frame_width, frame_height, 0, &this->window_ptr_, &this->window_renderer_ptr_);
+  SDL_SetRenderDrawColor(this->window_renderer_ptr_, 190, 255, 91, 100);
+  SDL_Rect background = { 0, 0, frame_width, frame_height};
+  SDL_RenderFillRect(this->window_renderer_ptr_, &background);
+
+  for (unsigned i = 0; i < n_sheep;i++)
+  {
+    sheep newSheep(this->window_renderer_ptr_);
+  }
+  for (unsigned j = 0; j < n_wolf; j++)
+    wolf neWolf(this->window_renderer_ptr_);
+
+  SDL_RenderPresent(this->window_renderer_ptr_); // equivalent a lupdate
+
+}
+
+application::~application()
+{
+  SDL_DestroyRenderer(this->window_renderer_ptr_);
+  SDL_DestroyWindow(this->window_ptr_);
+}
+
+sheep::sheep(SDL_Renderer* window_renderer_ptr_):animal("../media/sheep.png",window_renderer_ptr_)
+{
+  SDL_Rect rect = this->getAnimalRect();
+  SDL_RenderCopy(window_renderer_ptr_, this->getAnimalText(), NULL, &rect);
+}
+
+sheep::~sheep() = default;
+
+wolf::wolf(SDL_Renderer* window_renderer_ptr_):animal("../media/wolf.png",window_renderer_ptr_)
+{
+  SDL_Rect rect = this->getAnimalRect();
+  SDL_RenderCopy(window_renderer_ptr_, this->getAnimalText(), NULL, &rect);
+}
+
+wolf::~wolf() = default;
+
+animal::animal(const std::string& file_path, SDL_Renderer* window_renderer_ptr) {
+  // todo: The constructor has to load the sdl_surface that corresponds to the
+  // texture
+    this->animal_text = load_texture_for(file_path, window_renderer_ptr);
+    SDL_QueryTexture(this->animal_text, NULL, NULL, &this->animal_rect.w, &this->animal_rect.h);
+
+    this->animal_rect.x = get_random_int_in_range(frame_boundary, frame_width - frame_boundary);
+    this->animal_rect.y = get_random_int_in_range(frame_boundary, frame_height - frame_boundary);
+  };
+
+  
---
+++ Project_SDL_Part1_base/Project_SDL1.h
@@ -0,0 +1,124 @@
+﻿// SDL_Test.h: Includedatei für Include-Standardsystemdateien
+// oder projektspezifische Includedateien.
+
+#pragma once
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <optional>
+#include <vector>
+
+
+// Defintions
+constexpr double frame_rate = 60.0; // refresh rate
+constexpr double frame_time = 1. / frame_rate;
+constexpr unsigned frame_width = 1400; // Width of window in pixel
+constexpr unsigned frame_height = 900; // Height of window in pixel
+// Minimal distance of animals to the border
+// of the screen
+constexpr unsigned frame_boundary = 100;
+
+// Helper function to initialize SDL
+void init();
+
+class animal {
+private:
+
+SDL_Texture* animal_text = NULL;
+SDL_Rect animal_rect;
+
+public:
+  animal(const std::string& file_path, SDL_Renderer* window_renderer_ptr);
+
+  ~animal(){
+  // todo: Use the destructor to release memory and "clean up
+  // behind you"
+  SDL_DestroyTexture(this->animal_text); 
+  };
+  SDL_Texture* const getAnimalText(){
+    return this->animal_text;
+  }
+  SDL_Rect const getAnimalRect(){
+    return this->animal_rect;
+  }
+  //void draw(){}; // todo: Draw the animal on the screen <-> window_renderer_ptr.
+                 // Note that this function is not virtual, it does not depend
+                 // on the static type of the instance
+
+  //virtual void move(){} = 0; // todo: Animals move around, but in a different
+                             // fashion depending on which type of animal
+};
+
+// Insert here:
+// class sheep, derived from animal
+class sheep : public animal {
+  // todo
+  // Ctor
+  // Dtor
+  // implement functions that are purely virtual in base class
+  public:
+  sheep(SDL_Renderer* window_renderer_ptr_);
+  ~sheep();
+  };
+
+// Insert here:
+// class wolf, derived from animal
+// Use only sheep at first. Once the application works
+// for sheep you can add the wolves
+
+class wolf : public animal {
+  // todo
+  // Ctor
+  // Dtor
+  // implement functions that are purely virtual in base class
+  public:
+  wolf(SDL_Renderer* window_renderer_ptr_);
+  ~wolf();
+  };
+
+// The "ground" on which all the animals live (like the std::vector
+// in the zoo example).
+class ground {
+private:
+  // Attention, NON-OWNING ptr, again to the screen
+  SDL_Renderer* window_renderer_ptr_;
+  std::vector<std::unique_ptr<animal>> ListAnimal;
+  int counter = 0;
+  // Some attribute to store all the wolves and sheep
+  // here
+
+public:
+  //void AjouterDesAnimaux(std::)
+  ground(SDL_Surface* window_renderer_ptr); // todo: Ctor
+  ~ground() = default; // todo: Dtor, again for clean up (if necessary)
+  //void add_animal(some argument here); // todo: Add an animal
+
+
+  void update(); // todo: "refresh the screen": Move animals and draw them
+  // Possibly other methods, depends on your implementation
+};
+
+// The application class, which is in charge of generating the window
+class application {
+private:
+  // The following are OWNING ptrs
+  SDL_Window* window_ptr_ = NULL;
+  SDL_Renderer* window_renderer_ptr_ = NULL;
+  SDL_Event window_event_;
+
+  // Other attributes here, for example an instance of ground
+
+public:
+  application(unsigned n_sheep, unsigned n_wolf); // Ctor
+  ~application();                                 // dtor
+
+  // int loop(unsigned period); // main loop of the application.
+  //                            // this ensures that the screen is updated
+  //                            // at the correct rate.
+  //                            // See SDL_GetTicks() and SDL_Delay() to enforce a
+  //                            // duration the application should terminate after
+  //                            // 'period' seconds
+};
